e0b62f7f8dc4dfcf06bbff7ac05c13245eb40ba3
ultimecreation <naserbechata@gmail.com>
2024-12-09 16:12:33 +0100

    added @Repository annotation

--- backend/src/main/java/com/chatop/backend/repository/MessageRepository.java
+++ backend/src/main/java/com/chatop/backend/repository/MessageRepository.java
@@ -1,9 +1,11 @@
 package com.chatop.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chatop.backend.entity.Message;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 }
--- backend/src/main/java/com/chatop/backend/repository/RentalRepository.java
+++ backend/src/main/java/com/chatop/backend/repository/RentalRepository.java
@@ -1,9 +1,11 @@
 package com.chatop.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.chatop.backend.entity.Rental;
 
+@Repository
 public interface RentalRepository extends JpaRepository<Rental, Integer> {
 
 }
